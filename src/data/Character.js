da2078545ddc7c54897a50b31fc48da1da533749
radiumls <zl123405@163.com>
2022-12-29 21:42:32 +0800

    deck page can show character cards now

---
+++ src/data/Character.js
@@ -0,0 +1,16 @@
+// 七圣召唤的可出战角色的信息
+
+const charCards = {
+  5369: {
+    id: 5369,
+    name: '迪卢克',
+    icon: '5369.png',
+  }
+};
+
+const getCardsUrl = (charName) => {
+}
+
+export {
+  charCards,
+};
--- src/pages/Deck.js
+++ src/pages/Deck.js
@@ -1,10 +1,25 @@
 // 牌组页，用于展示牌组的信息
 import { Layout } from 'antd';
 // import backgroundPic from '../static/deck_background.png'
-import backgroundPic from '../static/deck_background2.png'
+import backgroundPic from '../static/deck_background2.png';
+import { charCards } from '../data/Character';
+import cards from '../static/cards/5369.png';
 
-const charCards = [];
+const cardsStaticPath = '/static/cards/'
 const deckCards = [];
+function CharCard({ id, style }) {
+  const char = charCards[id];
+  return <div style={style}>
+    <div style={{
+      width: '100%',
+      height: '100%',
+      backgroundImage: `url(${cards})`,
+      backgroundSize: 'cover',
+    }}>
+    </div>
+  </div>;
+}
+    // <img src={require(`${cardsStaticPath}${char.icon}`).default} alt={char.name}></img>
 function Deck({ children }) {
 
   // TODO: add background
@@ -17,8 +32,29 @@ function Deck({ children }) {
       height: '100vh',
       margin: '0 auto',
       backgroundSize: 'cover',
+      position: 'relative',
     }}>
-      卡组名称
+      <CharCard id='5369' style={{
+        width: '8.3vh',
+        height: '14.4vh',
+        position: 'absolute',
+        left: '21.7vh',
+        top: '11vh',
+      }}/>
+      <CharCard id='5369' style={{
+        width: '8.3vh',
+        height: '14.4vh',
+        position: 'absolute',
+        left: '31.7vh',
+        top: '11vh',
+      }}/>
+      <CharCard id='5369' style={{
+        width: '8.3vh',
+        height: '14.4vh',
+        position: 'absolute',
+        left: '41.7vh',
+        top: '11vh',
+      }}/>
     </div>
   </>
 }
