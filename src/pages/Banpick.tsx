b1d81c957b78eff375d53d5ec741dc33e52de6e5
radiumls <zl123405@163.com>
2023-02-03 21:31:16 +0800

    add Banpick page background

--- src/pages/Banpick.tsx
+++ src/pages/Banpick.tsx
@@ -1,9 +1,31 @@
 // bp模拟页
-import '../styles/deck.css';
+import { useState } from 'react';
+import { Button, Space } from 'antd';
+import '../styles/bp.css';
+// import bg from ''
+
+// 未来扩展一下i18n，先放个函数包一下
+const t = (i18n: string) => i18n;
 
 function Banpick() {
+  // 是否正在进行bp
+  const [ bping, setBping] = useState<Boolean>(false);
+  // 正在bp时展示bp的相关信息
+  if(bping) {
+    return <div className="bp-main-panel" style={{
+       backgroundImage: 'url("/static/bg/bp_bg.png")',
+    }}>
+      <Button onClick={() => setBping(false)}>{t('返回BP页')}</Button>
+      <br></br>
+      TODO: main banpick page
+    </div>;
+  }
   return <>
-    TODO: add banpick page
+    <Space>
+      <Button onClick={() => setBping(true)}>{t('开始单人BP')}</Button>
+      <Button>{t('修改BP配置')}</Button>
+    </Space>
+    <br></br>TODO: 增加BP的相关配置
   </>
 }
 
