af337f8550e7a3af857b06d5544b38043707ebae
radiumls <zl123405@163.com>
2023-02-11 18:30:12 +0800

    增加ICP备案信息到欢迎页面
    - 并不是必选项，如果需要，请手动修改.env相关文件

--- src/pages/Welcom.tsx
+++ src/pages/Welcom.tsx
@@ -1,9 +1,19 @@
 // 欢迎页，当作首页来用了
 import { Layout } from 'antd';
 
+const C_ICP_BEIAN = process.env.REACT_APP_ICP_BEIAN;
 function Wellcom() {
-  return <Layout>
-    欢迎来到七圣召唤模拟！
+  return <Layout style={{height: '100%'}}>
+    <div style={{height: '100%'}}>
+      <h2>
+      欢迎来到七圣召唤模拟！
+      </h2>
+    </div>
+    { C_ICP_BEIAN &&
+    <Layout.Footer style={{textAlign: 'center'}}>
+      <a href='http://beian.miit.gov.cn/' target='_blank'>{C_ICP_BEIAN}</a>
+    </Layout.Footer>
+    }
   </Layout>
 }
 
