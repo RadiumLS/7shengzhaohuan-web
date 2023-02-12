f9e53a3a22ddd14182f342c2731032cba5a9eb18
radiumls <zl123405@163.com>
2023-02-12 17:21:02 +0800

    import redux deps and add empty store

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
+import { Provider } from 'react-redux';
+import { myStore } from './redux';
 import { MyLayout } from './layout/MyLayout';
 import { MyRouter } from './MyRouter';
 
 function App() {
-  return <>
+  return <Provider store={myStore}>
     <MyLayout>
       <MyRouter></MyRouter>
     </MyLayout>
-  </>;
+  </Provider>;
 }
 
 export {
