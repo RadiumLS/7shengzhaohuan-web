d307eab7a7e0254e70750699769bd4a2a7d59d4b
radiumls <zl123405@163.com>
2023-12-22 12:15:19 +0800

    feat: 增加缩放的能力

--- src/components/play/movable_wrapper.tsx
+++ src/components/play/movable_wrapper.tsx
@@ -5,8 +5,8 @@ export type MovablePosition = {
   title?: string,
   top: string,
   left: string,
-  width?: string,
-  height?: string,
+  width: string,
+  height: string,
 }
 
 const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePosition}>> = (props) => {
@@ -16,10 +16,9 @@ const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePositio
   const [left, setLeft] = useState(defaultPostion.left);
   const [width, setWidth] = useState(defaultPostion.width);
   const [height, setHeight] = useState(defaultPostion.height);
-  // TODO: 增加可移动, 可放缩能力
+
   const [showBtn, setShowBtn] = useState(false);
   const [isMoving, setIsMoving] = useState(false);
-  const [isScaling, setIsScaling] = useState(false);
   // 记录点击时候的坐标
   const [downX, setDownX] = useState(0);
   const [downY, setDownY] = useState(0);
@@ -33,8 +32,13 @@ const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePositio
     setDownY(e.screenY);
     if(e.currentTarget.parentElement?.parentElement) {
       const ppElement = e.currentTarget.parentElement.parentElement;
-      setLeft(`${ppElement.offsetLeft}px`);
-      setTop(`${ppElement.offsetTop}px`);
+      if(e.currentTarget.id === 'move-btn') {
+        setLeft(`${ppElement.offsetLeft}px`);
+        setTop(`${ppElement.offsetTop}px`);
+      } else if(e.currentTarget.id === 'resize-btn') {
+        setWidth(`${ppElement.offsetWidth}px`);
+        setHeight(`${ppElement.offsetHeight}px`);
+      }
     }
   };
   const onMouseUp = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -45,11 +49,20 @@ const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePositio
     const deltaX = screenX - downX;
     const deltaY = screenY - downY;
     if(isMoving) {
-      setTop((currTop) => `${parseFloat(currTop) + deltaY}px`);
-      setLeft((currLeft) => `${parseFloat(currLeft) + deltaX}px`);
+      if(e.currentTarget.id === 'move-btn') {
+        setTop((currTop) => `${parseFloat(currTop) + deltaY}px`);
+        setLeft((currLeft) => `${parseFloat(currLeft) + deltaX}px`);
+      } else if(e.currentTarget.id === 'resize-btn') {
+        setWidth((currWidth) => `${parseFloat(currWidth) + deltaX}px`);
+        setHeight((currHeight) => `${parseFloat(currHeight) + deltaY}px`);
+      }
     }
     setDownX(screenX);
     setDownY(screenY);
+  }
+  const resetWidthHeight = () => {
+    setWidth(defaultPostion.width);
+    setHeight(defaultPostion.height);
   }
   return <div style={{
     position: 'absolute',
@@ -68,6 +81,7 @@ const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePositio
         onMouseLeave={() => setIsMoving(false)}
         onMouseUp={onMouseUp}
         className="p-4"
+        id="move-btn"
       >
         按住，慢慢拖动
       </button>
@@ -76,6 +90,21 @@ const MovableWrapper: React.FC<PropsWithChildren<{defaultPostion: MovablePositio
       </span>
     </p>}
     {children}
+    { showBtn && <p className="absolute -bottom-10 -right-2">
+      <button
+        onClick={resetWidthHeight}
+      >重置大小</button>
+      <button
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseLeave={() => setIsMoving(false)}
+        onMouseUp={onMouseUp}
+        className="ml-2 p-2"
+        id="resize-btn"
+      >
+        按住，慢慢缩放
+      </button>
+    </p>}
   </div>
 }
 
