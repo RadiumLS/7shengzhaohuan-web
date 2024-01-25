a28ea2950b71d6d2126820d5e9cebcb753e62f8d
radiumls <zl123405@163.com>
2024-01-25 17:13:55 +0800

    feat: 骰子选择区域增加检查骰子和需求费用是否匹配的功能

--- src/components/play/select_dice_area.tsx
+++ src/components/play/select_dice_area.tsx
@@ -5,17 +5,19 @@ import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { Dice, PlayerName, goNextPhase, rollDice, rerollDice } from "../../redux/play"
 import { useEffect, useState } from "react";
 import { OneDice } from "./roll_dice_area";
+import { checkCostMatch, spellCosts, spellDices } from "../../utils/dice";
 
 export const SelectDiceArea : React.FC<{player: PlayerName}> = (prop) => {
   const { player } = prop;
   const pName = player === 'boku' ? '本方' : '对方';
+  const [diceMatchCost, setDiceMatchCost] = useState<boolean>(false);
   const [selectedFlags, setSelectedFlags] = useState<boolean[]>([]);
   const switchSelectedIndex = (index: number) => {
     setSelectedFlags((prev) => {
       const newFlags = [...prev];
       newFlags[index] = !newFlags[index];
       return newFlags;
-    })
+    });
   }
   const [confirm, setConfirm] = useState<boolean>(false);
 
@@ -23,16 +25,34 @@ export const SelectDiceArea : React.FC<{player: PlayerName}> = (prop) => {
 
   const currPhase = useAppSelector((state) => state.play.currPhase);
   const dices = useAppSelector((state) => player === 'boku' ? state.play.bokuState.dice : state.play.kimiState.dice);
+  const requireCost =  useAppSelector((state) => player === 'boku' ? state.play.bokuState.requireCost : state.play.kimiState.requireCost);
   useEffect(() => {
     setSelectedFlags(new Array(dices.length).fill(false));
   }, [dices]);
   const confirmDice = () => {
+    // TODO: 检查是否满足消耗条件
+    // TODO: 注意还要检查充能
+    // TODO: 技能的触发是在这里进行的
   };
-  return <div className={`border-solid border-4 h-full ${player === 'boku' ? `border-boku bg-bgboku` : `border-kimi bg-bgkimi`}`}>
-    {confirm && <button onClick={confirmDice}>
-      {pName}骰子确认
-    </button>
+  useEffect(() => {
+    if(requireCost) {
+      const selectedDices = dices.filter((dice, diceIndex) => (selectedFlags[diceIndex]))
+      setDiceMatchCost(checkCostMatch(requireCost, selectedDices));
     }
+  }, [selectedFlags, dices, requireCost]);
+  return <div className={`releative border-solid border-4 h-full ${player === 'boku' ? `border-boku bg-bgboku` : `border-kimi bg-bgkimi`}`}>
+    {requireCost && <div className="absolute border-solid border-1 left-full w-20 bg-gray-500">
+      需要消耗: {spellCosts(requireCost)}
+      <br />
+      已经选择: {spellDices(dices.filter((dice, index) => selectedFlags[index]))}
+      <br />
+      是否匹配：{diceMatchCost ? '匹配' : '不匹配'}
+      <br />
+      <button onClick={confirmDice}>
+        {pName}确认消耗骰子
+      </button>
+    </div>}
+    
     <div className="w-16 flex flex-col">
       {dices.map((dice, index) => {
         return <div className={`w-16 h-16 ${selectedFlags[index] ? 'bg-red-500' : ''}`} onClick={() => switchSelectedIndex(index)}>
